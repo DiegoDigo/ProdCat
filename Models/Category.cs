63e4d2420348119970aaeab5b32fc4b71c394407
Diego <di3g0d0ming05@gmail.com>
2019-09-14 17:42:54 -0300

    commit inicial

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace ProductCatalog.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
